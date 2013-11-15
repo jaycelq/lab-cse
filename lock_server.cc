f0936fbd6063b95b834eb68e5d3e8a5b04a01448
Li Qiang <jaycelq@gmail.com>
2013-11-15 11:59:26 +0800

    part 1 step 1 finish

--- lock_server.cc
+++ lock_server.cc
@@ -6,9 +6,26 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 
+lock_held::lock_held()
+{
+  pthread_cond_init(&lock_cv, NULL);
+  lock_state = false;
+}
+
+lock_held::~lock_held()
+{
+  pthread_cond_destroy(&lock_cv);
+}
+
 lock_server::lock_server():
   nacquire (0)
 {
+  pthread_mutex_init(&lock, NULL);
+}
+
+lock_server::~lock_server()
+{
+  pthread_mutex_destroy(&lock);
 }
 
 lock_protocol::status
@@ -24,8 +41,24 @@ lock_protocol::status
 lock_server::acquire(int clt, lock_protocol::lockid_t lid, int &r)
 {
   lock_protocol::status ret = lock_protocol::OK;
-  printf("stat request from clt %d\n", clt);
+  std::map<lock_protocol::lockid_t, lock_held>::iterator it;
+  printf("acquire lock from clt %d and lid %llu\n", clt, lid);
   r = nacquire;
+  
+  pthread_mutex_lock(&lock);
+  it = lock_map.find(lid);
+  if(it == lock_map.end()){
+    lock_held lock_held_new;
+    lock_held_new.lock_state = true;
+    lock_map.insert(std::pair<lock_protocol::lockid_t, lock_held>(lid, lock_held_new));
+  }
+  else{
+    while(it->second.lock_state == true){
+      pthread_cond_wait(&(it->second.lock_cv), &lock);
+    }
+    it->second.lock_state = true;   
+  }
+  pthread_mutex_unlock(&lock);
   return ret;
 }
 
@@ -33,8 +66,23 @@ lock_protocol::status
 lock_server::release(int clt, lock_protocol::lockid_t lid, int &r)
 {
   lock_protocol::status ret = lock_protocol::OK;
-  printf("stat request from clt %d\n", clt);
+  std::map<lock_protocol::lockid_t, lock_held>::iterator it;
+  printf("release lock from clt %d and lid %llu\n", clt, lid);
   r = nacquire;
+
+  pthread_mutex_lock(&lock);
+  it = lock_map.find(lid);
+  if(it == lock_map.end()){
+    ret = lock_protocol::NOENT;
+  }
+  else{
+    if(it->second.lock_state ==false) ret = lock_protocol::IOERR;
+    else{
+      it->second.lock_state = false;
+      pthread_cond_signal(&(it->second.lock_cv));
+    }
+  }
+  pthread_mutex_unlock(&lock);
   return ret;
 }
 
--- lock_server.h
+++ lock_server.h
@@ -9,14 +9,24 @@
 #include "lock_client.h"
 #include "rpc.h"
 
-class lock_server {
+class lock_held {
+ public:
+  pthread_cond_t lock_cv;
+  bool lock_state;
+  lock_held();
+  ~lock_held();
+};
 
+class lock_server {
+ private:
+  std::map<lock_protocol::lockid_t, lock_held> lock_map;
+  pthread_mutex_t lock;
  protected:
   int nacquire;
 
  public:
   lock_server();
-  ~lock_server() {};
+  ~lock_server();
   lock_protocol::status stat(int clt, lock_protocol::lockid_t lid, int &);
   lock_protocol::status acquire(int clt, lock_protocol::lockid_t lid, int &);
   lock_protocol::status release(int clt, lock_protocol::lockid_t lid, int &);
